62d3d8b4ca9ed09b815b5012371e48582ca56ada
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2024-01-14 13:44:12 +0500

    Add project files.

---
+++ WpfApp9/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System.Media;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\C.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\F.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\C_s.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\D.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\D_s.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\E.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\F_s.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\G.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\G_s.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\A.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\Bb.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\B.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\C_s.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\C_s1.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\D1.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\D_s1.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_16(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\E.wav");
+            soundPlayer.Play();
+        }
+
+        private void Button_Click_17(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\F1.wav");
+            soundPlayer.Play();
+        }
+    }
+}
