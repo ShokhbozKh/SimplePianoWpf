b6f6ffbe57854ebbd47f380d9bb2e5aa10ff3d90
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2024-01-14 19:50:04 +0500

    hello

--- WpfApp9/MainWindow.xaml.cs
+++ WpfApp9/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,6 +21,108 @@ namespace WpfApp9
         public MainWindow()
         {
             InitializeComponent();
+            //hello codes
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e) /// KEYBOARD ////////////
+        {
+            if (e.Key == Key.A)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\A.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.B)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\B.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.C)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\C.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.Q)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\BB.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.C)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\C.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.W)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\C_s.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.R)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\C_s1.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.T)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\C1.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.D)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\D.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.Y)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\D_s.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.U)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\D_s1.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.I)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\D1.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.E)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\E.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.O)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\E1.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.F)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\F.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.P)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\F_s.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.M)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\F1.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.G)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\G.wav");
+                soundPlayer.Play();
+            }
+            if (e.Key == Key.N)
+            {
+                SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\G_s.wav");
+                soundPlayer.Play();
+            }
+
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -129,5 +232,7 @@ namespace WpfApp9
             SoundPlayer soundPlayer = new SoundPlayer(@"D:\PDP .Net\C# codes\WpfApp9\WpfApp9\Resources\F1.wav");
             soundPlayer.Play();
         }
+
+
     }
 }
